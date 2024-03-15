1ecbe4c659641a0dc010b918f8feba3e23f2e531
ChitanZignuts <chintanr@zignuts.com>
2024-03-15 16:53:35 +0530

    feat: :zap: Initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <NuxtLayout>
+    <header>
+      <NuxtLink to="/test">Test</NuxtLink>
+      <NuxtLink to="/">Home</NuxtLink>
+    </header>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
